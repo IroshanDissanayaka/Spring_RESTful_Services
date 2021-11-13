aa3a2c3dad24e4e1bef9be648f7efbafcf3b967d
dissanayaka.ip@gmail.com <dissanayaka.ip@gmail.com>
2021-11-13 19:56:17 +0000

    202111131956

--- src/main/java/com/iroshan/springrestfulservices/LoadDatabase.java
+++ src/main/java/com/iroshan/springrestfulservices/LoadDatabase.java
@@ -1,4 +1,29 @@
 package com.iroshan.springrestfulservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring @Configuration annotation helps in Spring annotation based configuration.
+ * @Configuration annotation indicates that a class declares one or more
+ * @Bean methods and may be processed by the Spring container to generate bean
+ * definitions and service requests for those beans at runtime.
+ */
+
+@Configuration
 public class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+
 }
